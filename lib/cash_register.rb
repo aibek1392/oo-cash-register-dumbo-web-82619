ae9c2e0d8473676d24e3ad5f9665307206176f0c
Aibek Ozhorov <ozhorov@gmail.com>
2019-09-03 00:23:28 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -9,26 +9,23 @@ class CashRegister
     @discount = discount
     @items = []
   end
-  def add_item(title,price)
-    i=0 
-    i+=1
+ 
+
+
+  def add_item(title,price,quantity = 1)
+    if quantity>1
+      i=0
+      while i<quantity
+        @items << title
+        i+=1
+      end
+    else
+      @items << title
+    end
+    @total += price*quantity
+    @last_transaction_amount = @total
+    @total
   end
-
-
-  # def add_item(title,price,quantity = 1)
-  #   if quantity>1
-  #     i=0
-  #     while i<quantity
-  #       @items << title
-  #       i+=1
-  #     end
-  #   else
-  #     @items << title
-  #   end
-  #   @total += price*quantity
-  #   @last_transaction_amount = @total
-  #   @total
-  # end
 
   # def apply_discount()
   #   if @discount > 0
