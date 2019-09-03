24ae77454f0c49383be661031ac8e96d52588fd1
Aibek Ozhorov <ozhorov@gmail.com>
2019-09-03 00:18:14 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,14 +1,15 @@
 
 
 class CashRegister
+  attr_accessor :total, :discount, :last_transaction_amount, :items
+
 
 def initialize(total = 0,discount)
   @total = total
   @discount = discount
 end
 
-  # attr_accessor :total, :discount, :last_transaction_amount, :items
-
+  
   # def initialize(discount = 0)
   #   @total = 0
   #   @discount = discount
